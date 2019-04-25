79aaa7bdbaf0b46066c0d0c6b47114fa29222d78
polamokh <polamokh10@hotmail.com>
2019-04-25 13:20:43 +0200

    Initial commit.

---
+++ OODP-ShoppingManagementSystem-SourceCode/src/main/Frm_Main.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class Frm_Main {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Frm_Main window = new Frm_Main();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Frm_Main() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
