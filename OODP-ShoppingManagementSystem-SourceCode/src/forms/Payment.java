5091d0b1676c74d8d900ee8492969c65791d31e8
polamokh <polamokh10@hotmail.com>
2019-05-01 15:17:41 +0200

    Fix .classpath file.

--- OODP-ShoppingManagementSystem-SourceCode/src/forms/Payment.java
+++ OODP-ShoppingManagementSystem-SourceCode/src/forms/Payment.java
@@ -1,24 +1,14 @@
 package forms;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-
-import classes.Card;
-import classes.Category;
-import classes.MenuComponent;
-
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-
 import java.awt.Font;
-import javax.swing.JRadioButton;
 
 public class Payment extends JFrame {
 
